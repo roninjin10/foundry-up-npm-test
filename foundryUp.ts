796c727ee95a9e84a4045a6d4524d926bed6d966
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-02-12 09:00:47 -0800

    add logs

--- foundryUp.ts
+++ foundryUp.ts
@@ -1,5 +1,7 @@
 import * as foundryup from "@foundry-rs/easy-foundryup";
 import childProcess from "child_process";
 
+console.log("running curl -L https://foundry.paradigm.xyz | bash");
 childProcess.execSync("curl -L https://foundry.paradigm.xyz | bash");
+console.log("running foundryup");
 foundryup.run(true);
