373b94c7f047aca1307ae59c7e0833a4bb0eec11
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-02-12 08:59:19 -0800

    download foundryup

--- foundryUp.ts
+++ foundryUp.ts
@@ -1,3 +1,5 @@
 import * as foundryup from "@foundry-rs/easy-foundryup";
+import childProcess from "child_process";
 
+childProcess.execSync("curl -L https://foundry.paradigm.xyz | bash");
 foundryup.run(true);
