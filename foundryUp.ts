48af844699d970633491193035ed04dd38f78986
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-02-12 09:08:47 -0800

    use a script

--- foundryUp.ts
+++
@@ -1,9 +0,0 @@
-import * as foundryup from "@foundry-rs/easy-foundryup";
-import childProcess from "child_process";
-
-console.log("running curl -L https://foundry.paradigm.xyz | bash");
-childProcess.execSync("curl -L https://foundry.paradigm.xyz | bash");
-console.log("sourcing...");
-childProcess.execSync("source $HOME/.bashrc");
-console.log("running foundryup");
-childProcess.execSync("foundryup");
